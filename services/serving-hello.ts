1a02dee388860e7fcf3e727b4e5210582163899a
graemen16 <graemen16@gmail.com>
2023-06-02 18:07:55 +0100

    more trial and error on env

--- services/serving-hello.ts
+++ services/serving-hello.ts
@@ -4,6 +4,8 @@ import { config } from "../config";
 export async function helloText(context: Context): Promise<string> {
     const someText = process.env.HOST;
     context.log("saying Hello from Graeme's function : " + someText)
-    context.log(config.db)
+    var var1 = config.db.database
+    
+    //context.log(config.db)
     return "Hello from Graeme's function"
 }
